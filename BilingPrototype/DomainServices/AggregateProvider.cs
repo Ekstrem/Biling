8c737ffe63815200302bb56fdf6aca63e6d252fc
Evgenei Voitov <about47@yandex.ru>
2021-11-02 09:50:26 +0300

    Провайдер агрегатов

---
+++ BilingPrototype/DomainServices/AggregateProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using Domain.Abstraction;
+using Hive.SeedWorks.TacticalPatterns;
+
+namespace DomainServices
+{
+    public class AggregateProvider : IAggregateProvider<IBilling, IBillingAnemicModel>
+    {
+        private readonly IBillingAggregateRepository _repository;
+
+        public AggregateProvider(IBillingAggregateRepository repository) => _repository = repository;
+
+        public IBillingAnemicModel GetAggregate(Guid id, long version)
+            => _repository
+                .GetByIdAndVersion(id, version, default)
+                .GetAwaiter()
+                .GetResult();
+
+        public Task<IBillingAnemicModel> GetAggregateAsync(Guid id, long version, CancellationToken cancellationToken)
+            => _repository.GetByIdAndVersion(id, version, cancellationToken);
+
+        public IBillingAnemicModel GetAggregate(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBillingAnemicModel GetAggregateAsync(Guid id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BilingPrototype/DomainServices/IBillingAggregateRepository.cs
@@ -0,0 +1,5 @@
+﻿using Domain;
+using Domain.Abstraction;
+using Hive.SeedWorks.TacticalPatterns;
+
+public interface IBillingAggregateRepository : IAnemicModelRepository<IBilling, IBillingAnemicModel> {}
